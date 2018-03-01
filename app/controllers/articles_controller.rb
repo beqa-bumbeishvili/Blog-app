d804761de57ca7735daba5da09d648c065499409
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2018-03-01 23:42:06 +0400

    make project macOS compatible

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,62 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article created like a charm'
+      redirect_to :action => "show", :id => @article.id
+    else
+      redirect_to 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was destroyed'
+
+    redirect_to articles_path
+  end
+
+  def welcome
+
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+end
